c4936331e334b304706adcdd4fc3c0ac785e9e5a
Omkar Jadhav <jadhavomkarb4u@gmail.com>
2024-06-01 00:13:44 +0530

    remove unnecessary files

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\jadha\OneDrive\Documents\Downloads\CA_CP\env
+set VIRTUAL_ENV=C:\Users\jadha\OneDrive\Documents\Downloads\Algorithmic Trading Simulation\env
 
 if not defined PROMPT set PROMPT=$P$G
 
